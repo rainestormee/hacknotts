7533e83f6d4a383c285f421dd39f9c0f2fc4f335
Gitjiggywidit <dom_41@hotmail.co.uk>
2020-11-28 18:43:25 +0000

    Update CodeGenerator.py

--- Dom/CodeGenerator.py
+++ Dom/CodeGenerator.py
@@ -8,24 +8,23 @@ class CodeGenerator:
         
     dbconnect = mysql.connector.connect(user = "root", password = "alexandre", host = "127.0.0.1", database = "hacknotts")
     cursor = dbconnect.cursor()
-    #cursor.execute("SELECT * FROM auth_codes")
-    
-  # auth_code_res = cursor.fetchall()
-    
-    #for a in auth_code_res:
-      #  print(a)
-    
-    cursor.execute("SELECT * FROM users")
+   
+    cursor.execute("SELECT * FROM users WHERE")
     
     users = cursor.fetchall()
     
     for u in users:
-        print(u)
+        print(u[0])
 
 
 
 
-        
+   #cursor.execute("SELECT * FROM auth_codes")
+    
+   #auth_code_res = cursor.fetchall()
+    
+       #for a in auth_code_res:
+           #print(a)       
             
 
     
