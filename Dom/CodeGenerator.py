aa9c3fade05f2cf5be35dfedd28def4a1ec51820
Gitjiggywidit <dom_41@hotmail.co.uk>
2020-11-28 20:14:14 +0000

    Update CodeGenerator.py

--- Dom/CodeGenerator.py
+++ Dom/CodeGenerator.py
@@ -3,26 +3,50 @@ import os, random, requests, mysql.connector
 
 class CodeGenerator:
         
-    def __init__(self, id):
-        self.id = id
+    def __init__(self, ID, auth_codes, target_user):
+        self.ID = ID
+        db_connect = mysql.connector.connect(user = "root", password = "alexandre", host = "127.0.0.1", database = "hacknotts")
+        cursor = db_connect.cursor()
+        cursor.execute("SELECT * FROM auth_codes")
+        self.auth_codes = cursor.fetchall()
+        self.target_user = auth_codes.index(ID)
         
-    dbconnect = mysql.connector.connect(user = "root", password = "alexandre", host = "127.0.0.1", database = "hacknotts")
-    cursor = dbconnect.cursor()
-    cursor.execute("SELECT * FROM auth_codes")
-    auth_codes = cursor.fetchall()
+    #    self.generated_code = create_code()
+    
+    def generate_code():
+        r = random()
+        code = ""
+        for i in range(5):
+            rand_num = r.randint(0, 9)
+            code + str(rand_num)
+        return code
+    
+    def check_code(self, code):
+        if code in self.target_user[2]:
+            return False
+            
         
-    for c in auth_codes:
+    #def search_id(self.id):
+        
+    
+    
+    for c in get_auth_codes_table():
         print(c)
     
-        
+    
+    def get_auth_codes_table(self):
+        return self.auth_codes
+    
+    
+    # create table auth_codes (id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, user_id INT NOT NULL, code VARCHAR(6) NOT NULL, used TINYINT(1) DEFAULT 0 NOT NULL);
 
    
-    cursor.execute("SELECT * FROM users")
+    # cursor.execute("SELECT * FROM users")
     
-    users = cursor.fetchall()
+    # users = cursor.fetchall()
     
-    for u in users:
-        print(u)
+    # for u in users:
+    #     print(u)
 
         
 
@@ -33,7 +57,10 @@ class CodeGenerator:
 #     print(c)
     
 
-   
+# check auth_codes for the id
+# generate a code for the id
+# check if the code is already use
+# if not, insert the code into the auth_codes[2] (which is code)
             
 
     
