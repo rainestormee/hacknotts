7ff36bf31cc6de52b7af002f9b87f874fe68640d
jadon Barnes <jadonbarnes@jadons-MacBook-Pro.local>
2020-11-28 18:34:30 +0000

    first draft backend

---
+++ hacknotts/Authentication/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    user = models.ForeignKey(User, unique=False)
