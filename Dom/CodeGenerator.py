8dd7af589ad71ebdc10fb97100fed5f2168ff064
Gitjiggywidit <dom_41@hotmail.co.uk>
2020-11-28 18:10:49 +0000

    Update CodeGenerator 3

--- Dom/CodeGenerator.py
+++ Dom/CodeGenerator.py
@@ -5,18 +5,28 @@ class CodeGenerator:
         
     def __init__(self, id):
         self.id = id
-    
-
-   # def create_code(self.id):
-    #    code = ""
-     #   for i in range(5):
-      #      random = random
         
+    dbconnect = mysql.connector.connect(user = "root", password = "alexandre", host = "127.0.0.1", database = "hacknotts")
+    cursor = dbconnect.cursor()
+    cursor.execute("CREATE TABLE auth_codes (id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, user_id INT NOT NULL, code VARCHAR(6) NOT NULL, used TINYINT(1) DEFAULT 0 NOT NULL);")
+    cursor.execute("SHOW TABLES")
+    
+    for c in cursor:
+        print(c)
+    
+    
+    #users = cursor.execute("QUERY", (auth_codes, c))
+
+
+
+
+
         
             
-    dbconnect = mysql.connector.connect(user = "root", password = "alexandre", host = "127.0.0.1", database = "hacknotts")    
-    #response = requests.get(dbconnect)
 
-gen = CodeGenerator(1)
+    
 
-print(gen.response)
+    
+
+
+
