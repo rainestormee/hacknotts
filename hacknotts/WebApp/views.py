9e712b2dbbffb50bfc4a578185a6e666b97446bf
jadon Barnes <jadonbarnes@jadons-MacBook-Pro.local>
2020-11-29 09:04:13 +0000

    passing data through to accounts.html

--- hacknotts/WebApp/views.py
+++ hacknotts/WebApp/views.py
@@ -35,10 +35,15 @@ def index(request):
             transaction_list.append(transaction['timestamp'])
             transaction_list.append(transaction['status'])
             transactions_list.append(transaction_list)
-
         json_list = json.dumps(transactions_list)
 
-        context = {'json_list':json_list}
+        apiResult = ApiGet("https://sandbox.capitalone.co.uk/developer-services-platform-pr/api/data/accounts/" + str(account.accountID))
+        accountResult = apiResult.json()["Accounts"][0]
+        name = str(accountResult['firstname'] + accountResult['lastname'])
+        creditScore = accountResult['creditScore']
+        context = {'json_list':json_list,
+                   'name':name,
+                   'creditscore': creditScore}
     return HttpResponse(template.render(context, request))
 
 
