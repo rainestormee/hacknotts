5ba8c66692fabd886f009b2bb8385106f3071152
Gitjiggywidit <dom_41@hotmail.co.uk>
2020-11-29 07:26:10 +0000

    Update CodeGenerator.py

--- Dom/CodeGenerator.py
+++ Dom/CodeGenerator.py
@@ -29,7 +29,7 @@ class CodeGenerator:
     def send_message(self):
         account_sid = 'AC981838466c123165f5a99c5913488181'
         auth_token = 'f33080514f1b3e9cd3b7c6e6dc92748b'
-        messaging_sid = 'PN77be9d18bceff01b2e184040f98c516e'
+        messaging_sid = '+447723412253' #'PN77be9d18bceff01b2e184040f98c516e'
         the_body = self.get_auth_code()
         try:            
             client = Client(account_sid, auth_token)
