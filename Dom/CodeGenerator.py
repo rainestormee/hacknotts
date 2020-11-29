e9205e88f73ef676112fcc7cbe1f2ff80a0a5f76
Gitjiggywidit <dom_41@hotmail.co.uk>
2020-11-29 04:10:57 +0000

    Update CodeGenerator.py

--- Dom/CodeGenerator.py
+++ Dom/CodeGenerator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import mysql.connector
 import random, sys
-# from twilio.rest import Client
+from twilio.rest import Client
 
 class CodeGenerator:
 
@@ -21,18 +21,26 @@ class CodeGenerator:
             self.users = cursor.fetchall()
             self.target_user = self.users[ID-1]
             self.phone_number = self.target_user[3]
-            print(self.phone_number)
+
         except mysql.connector.InterfaceError:
             print("Could not connect to database.", file=sys.stderr)
             
-    # account_sid = os.environ["AC981838466c123165f5a99c5913488181"]
-    # auth_token = os.environ["3ccf69ca542b8cbf33a1986ce759b4bf"]
+    account_sid = os.environ["AC981838466c123165f5a99c5913488181"]
+    auth_token = os.environ["3ccf69ca542b8cbf33a1986ce759b4bf"]
     
-    # def send_message(account_sid, auth_token):
-    #     client = Client(account_sid, auth_token)
-    #     client.messages.create(
+    def get_code(self):
+        cursor = self.db_connect.cursor(buffered=True)
+        cursor.execute("SELECT * FROM auth_codes;")
+        return self.auth_codes[ID-1][2]
+    
+    def send_message(self, account_sid, auth_token):
+        client = Client(account_sid, auth_token)
+        client.messages.create(
+            to = self.phone_number,
+            from_ = ""
+            body = get_code
             
-
+    
 
     def get_auth_codes_table(self):
         return self.auth_codes
@@ -76,7 +84,9 @@ class CodeGenerator:
     
 if __name__ == "__main__":
     CD = CodeGenerator(1)
+    print(CD.get_code())
     CD.update_auth_code()
+    print("\n" + str(CD.get_code()))
     cursor = CD.db_connect.cursor(buffered=True)
     cursor.execute("SELECT * FROM auth_codes;")
     upd_auth_codes = cursor.fetchall()
