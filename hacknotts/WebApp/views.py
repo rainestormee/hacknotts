b9effc6b4d86d1eceb27057891f2cdb3284fabd3
jadon Barnes <jadonbarnes@jadons-MacBook-Pro.local>
2020-11-29 03:10:26 +0000

    account updated

--- hacknotts/WebApp/views.py
+++ hacknotts/WebApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, loader
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import user_passes_test
@@ -8,11 +8,16 @@ from .api import *
 
 # Create your views here.
 @login_required
-def index(request):
+def index2(request):
     content = ApiGet('https://sandbox.capitalone.co.uk/developer-services-platform-pr/api/data/accounts')
     return HttpResponse(content.text)
 
 
+@login_required
+def index(request):
+    template = loader.get_template('WebApp/account.html')
+    context = { }
+    return HttpResponse(template.render(context, request))
 
 
 
